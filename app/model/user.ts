aca4276af877ae7200782b5a0ea33ada56050819
lyw <you@example.com>
2022-03-16 21:56:06 +0800

    添加 toJSON 以及 trasnform 隐藏固定信息

--- app/model/user.ts
+++ app/model/user.ts
@@ -20,7 +20,14 @@ function initUserModel(app: Application) {
     picture: { type: String },
     email: { type: String },
     phoneNumber: { type: String },
-  }, { timestamps: true })
+  }, { timestamps: true,
+    toJSON: {
+      transform(_doc, ret) {
+        delete ret.password
+        delete ret.__v
+      }
+    }
+  })
   return app.mongoose.model<UserProps>('User', UserSchema)
 }
 
