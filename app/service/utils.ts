ace0c9ade9f82aba1f9e049669abbff8e650bc9c
lyw <you@example.com>
2022-03-20 00:43:56 +0800

    添加 body 样式

--- app/service/utils.ts
+++ app/service/utils.ts
@@ -3,8 +3,18 @@ import { createSSRApp } from 'vue'
 import LegoComponents from 'lego-components'
 import { renderToString, renderToNodeStream } from '@vue/server-renderer'
 export default class UserService extends Service {
+  propsToStyle(props = {}) {
+    const keys = Object.keys(props)
+    const styleArr = keys.map(key => {
+      const formatKey = key.replace(/[A-Z]/g, c => `-${c.toLocaleLowerCase()}`)
+      // fontSize -> font-size
+      const value = props[key]
+      return `${formatKey}: ${value}`
+    })
+    return styleArr.join(';')
+  }
   async renderToPageData(query: { id: string, uuid: string }) {
-    const work = await this.ctx.model.Work.findOne(query).lean()
+    const work = await this.ctx.model.Work.findOne(query as any).lean()
     if (!work) {
       throw new Error('work not exsit')
     }
@@ -19,10 +29,12 @@ export default class UserService extends Service {
     })
     vueApp.use(LegoComponents)
     const html = await renderToString(vueApp)
+    const bodyStyle = this.propsToStyle(content && content.props)
     return {
       html,
       title,
-      desc
+      desc,
+      bodyStyle
     }
   }
 }
