fa9ef822034772fb59fd23bfadfb055ca0f70abe
lyw <lywRobot@163.com>
2022-03-23 20:41:20 +0800

    xx

--- config/config.prod.ts
+++ config/config.prod.ts
@@ -4,7 +4,7 @@ export default () => {
   const config: PowerPartial<EggAppConfig> = {};
   config.baseUrl = 'prod.url'
   config.mongoose = {
-    url: 'mongodb://fun5-mongo:27018/fun5'
+    url: 'mongodb://mongo:27018/fun5'
   }
   return config;
 };
